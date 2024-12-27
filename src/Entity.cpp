5435af892345cbbedde57ad0c2be48083d0a3801
mohamed almahdi <almahdimohamed627@gmail.com>
2024-12-27 14:53:35 +0300

    extracted DrawGlScene to enhance readablity

--- src/Entity.cpp
+++ src/Entity.cpp
@@ -1,7 +1,7 @@
 #include "Entity.h"
 
 Entity::Entity()
-    : translate_x(0), translate_y(0), translate_z(0),
+    : translate_x(Ex), translate_y(E), translate_z(Ez),
       rotate_x(0), rotate_y(0), rotate_z(0),
       scale_x(1), scale_y(1), scale_z(1),
       red(0), green(0), blue(0), alpha(255) {}
