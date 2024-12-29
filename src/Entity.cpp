dbad5868bdca2cb3730ace5c2b9fcc2ee033a541
mohamed almahdi <almahdimohamed627@gmail.com>
2024-12-29 19:51:57 +0300

    fixed scaling on x.. made translation amount smaller

--- src/Entity.cpp
+++ src/Entity.cpp
@@ -17,7 +17,7 @@ Entity::Entity()
     count += 1;
 }
 
-Entity::Entity(Entity* e)
+Entity::Entity(Entity *e)
     : translate_x(e->translate_x), translate_y(e->translate_y), translate_z(e->translate_z),
       rotate_x(e->rotate_x), rotate_y(e->rotate_y), rotate_z(e->rotate_z),
       scale_x(e->scale_x), scale_y(e->scale_y), scale_z(e->scale_z),
@@ -28,7 +28,8 @@ Entity::Entity(Entity* e)
     count += 1;
 }
 
-Entity::~Entity() {
+Entity::~Entity()
+{
     count--;
 }
 
@@ -54,8 +55,9 @@ void Entity::display()
     glPopMatrix();
 }
 
-void Entity::displayInfo() {
-    char buffer[100];    // Allocate a buffer for the formatted string
+void Entity::displayInfo()
+{
+    char buffer[100];                      // Allocate a buffer for the formatted string
     sprintf(buffer, "name:: %s", "hello"); // Format the string
     renderBitmapText(0.8, -0.7, buffer, GLUT_BITMAP_HELVETICA_18);
 }
@@ -78,53 +80,62 @@ int Entity::selected(int index)
     return selectedIndex;
 }
 
-void Entity::setColor(int red, int green, int blue, int alpha) {
-    this->red=red;
-    this->green=green;
-    this->blue=blue;
-    this->alpha=alpha;
+void Entity::setColor(int red, int green, int blue, int alpha)
+{
+    this->red = red;
+    this->green = green;
+    this->blue = blue;
+    this->alpha = alpha;
 }
 
-void Entity::transform(char transformation, bool x, bool y, bool z, int amount) {
+void Entity::transform(char transformation, bool x, bool y, bool z, int amount)
+{
     switch (transformation)
     {
     case 't':
-        if(x) {
-            translate_x+=amount;
+        if (x)
+        {
+            translate_x += amount * 0.1;
         }
-        if(y) {
-            translate_y+=amount;
+        if (y)
+        {
+            translate_y += amount * 0.1;
         }
-        if(z) {
-            translate_z+=amount;
+        if (z)
+        {
+            translate_z += amount * 0.1;
         }
         break;
     case 'r':
-        if(x) {
-            rotate_x+=amount;
+        if (x)
+        {
+            rotate_x += amount;
         }
-        if(y) {
-            rotate_y+=amount;
+        if (y)
+        {
+            rotate_y += amount;
         }
-        if(z) {
-            rotate_z+=amount;
+        if (z)
+        {
+            rotate_z += amount;
         }
         break;
     case 's':
-        if(x) {
-            scale_x+=amount*0.01;
+        if (x)
+        {
+            scale_x += amount * 0.1;
         }
-        if(y) {
-            scale_y+=amount*0.1;
+        if (y)
+        {
+            scale_y += amount * 0.1;
         }
-        if(z) {
-            scale_z+=amount*0.1;
+        if (z)
+        {
+            scale_z += amount * 0.1;
         }
         break;
-    
+
     default:
         break;
     }
-
-
 }
